6b93569cf773fe1f7cff30f8821d491b447ea5cc
Vitaly Bakuntsev <49323079+bacugniano@users.noreply.github.com>
2022-06-29 15:56:41 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports = function toReadable (number) {
   let arrayTeen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
   let arrayTen = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   let numStr = number.toString().split('');
-
   if (number >= 0 && number <= 10) {
     return arrayOne[number];
   }
@@ -12,21 +11,23 @@ module.exports = function toReadable (number) {
   if (number > 10 && number < 20) {
     return arrayTeen[number - 10]; 
   }
-
   if (number >=20 && number < 100) {
     if (numStr[1] == 0) {
       return arrayTen[numStr[0]];
     }
     else return `${arrayTen[numStr[0]]} ${arrayOne[numStr[1]]}`
   }
-
   if (number >= 100 && number < 1000) {
     if (numStr[1] == 0 && numStr[2] == 0) {
       return `${arrayOne[numStr[0]]} hundred`
     }
 
+    else if (numStr[1] > 1 && numStr[2] == 0) {
+      return `${arrayOne[numStr[0]]} hundred ${arrayTeen[numStr[1]]}`
+    }
+
     else if (numStr[1] == 1 && numStr[2] == 0) {
-      return `${arrayOne[numStr[0]]} hundred ${arrayTeen[0]}`
+      return `${arrayOne[numStr[0]]} hundred ten`
     }
 
     else if (numStr[1] == 0 && numStr[2] > 0) {
